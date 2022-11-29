5a83a1d282a25f89f8729220d8c160b17100fa15
tulski <me@tulski.com>
2022-11-29 21:04:00 +0100

    spring initializer starter

---
+++ src/main/java/com/tulski/bekom/BekomDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tulski.bekom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BekomDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BekomDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tulski/bekom/BekomDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tulski.bekom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BekomDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
